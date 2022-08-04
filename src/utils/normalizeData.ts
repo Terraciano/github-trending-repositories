261e33848c3619a2f3bb959f476f8fb757d5dfdc
Juan Pereyra <juan.pereyra@mediamonks.com>
2022-08-04 18:41:13 -0300

    remove body braces from normalizeData

--- src/utils/normalizeData.ts
+++ src/utils/normalizeData.ts
@@ -4,15 +4,13 @@ export const normalizeData = (
   items: Array<responseItem> | undefined
 ): Array<Repository> | undefined => {
   if (!items) return undefined;
-  return items.map((repository) => {
-    return {
-      name: repository.name,
-      date: repository.created_at,
-      language: repository.language,
-      description: repository.description,
-      url: repository.html_url,
-      stars: repository.stargazers_count,
-      id: repository.id,
-    };
-  });
+  return items.map((repository) => ({
+    name: repository.name,
+    date: repository.created_at,
+    language: repository.language,
+    description: repository.description,
+    url: repository.html_url,
+    stars: repository.stargazers_count,
+    id: repository.id,
+  }));
 };
